101184291ac071464229cb886315aa4cf528b136
mirthful-concept-6030 <jchristinem@gmail.com>
2020-06-27 22:15:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     puts "Please select an unoccupied square."
-  elsif index != between?(0, 8)
+  elsif index != between?(0,8)
     puts "Please select a valid square on the board."
   else
     puts "Great move!"
