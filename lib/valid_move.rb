40d54486e434bf958d1541bd210f7e822dc7f201
mirthful-concept-6030 <jchristinem@gmail.com>
2020-06-27 22:10:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  if position_taken?
+    puts "Please select an unoccupied square."
+  elsif index != between(0, 8)
+    puts "Please select a valid square on the board."
+  else
+    puts "Great move!"
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
